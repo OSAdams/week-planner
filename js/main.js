9dee24d753746266080aa6f2e27e71aac30743a3
Owen Adams <owenadams.main@gmail.com>
2022-04-21 11:43:58 -0400

    add ids to day buttons. remove p element. query dom for week days. add event listener to week days. #1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 /* global data */
 /* exported data */
+
+const weekDays = document.querySelector('#week-days');
+
+let dataView = '';
+
+weekDays.addEventListener('click', eventTarget);
+
+function eventTarget(event) {
+  if (event.target.getAttribute('class') === 'day fl-ai-jc-cen') {
+    location.hash = '#' + event.target.getAttribute('id');
+    dataView = event.target.getAttribute('id');
+    // eslint-disable-next-line
+    console.log(dataView);
+  }
+}
