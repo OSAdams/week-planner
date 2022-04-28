ba572b646db1ceaaeabf4bb26cd811d03b182a4f
Owen Adams <owenadams.main@gmail.com>
2022-04-27 22:55:55 -0400

    stringify input form values to local storage. add functionality on page load to parse stringified object and update the value of weeklyPlannerData. change weeklyPlannerData to let variable. #2

--- js/data.js
+++ js/data.js
@@ -1,8 +1,18 @@
 /* exported weeklyPlannerData */
+const savedWeeklyPlannerDataJSON = localStorage.getItem('weekly-planner-local-storage');
 
-const weeklyPlannerData = {
+let weeklyPlannerData = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+if (savedWeeklyPlannerDataJSON !== null) {
+  weeklyPlannerData = JSON.parse(savedWeeklyPlannerDataJSON);
+}
+
+window.addEventListener('beforeunload', event => {
+  const weeklyPlannerDataJSON = JSON.stringify(weeklyPlannerData);
+  localStorage.setItem('weekly-planner-local-storage', weeklyPlannerDataJSON);
+});
